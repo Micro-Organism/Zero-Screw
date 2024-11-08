258963a8b0f57aa77c56c83933be4e0ca0bdf183
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-08 10:11:55 +0800

    feat: springboot 集成 screw

---
+++ zero-screw-boot/src/test/java/com/zero/screw/ZeroScrewBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.screw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroScrewBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
